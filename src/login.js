d246979368abf88b803067a7f47075581f0f2961
Fong <106387748+FongP@users.noreply.github.com>
2022-07-26 17:46:59 +0700

    delete stupid puppeteer and add import server

--- src/login.js
+++ src/login.js
@@ -2,8 +2,6 @@ import login from "fca-unofficial-force"
 import fs from "fs"
 import path from "path"
 import npmlog from "npmlog"
-import puppeteer from "puppeteer"
-import twofactor from "node-2fa"
 import {
 	int,
 	er,
@@ -13,7 +11,6 @@ import {
 
 export async function facebook() {
 	if (global.config.login.facebook.enablefb == true) {
-		if (global.config.login.facebook.enablefbstate == true) {
 		const filelogin = fs.readdirSync(path.join( "loginfile"))
 		if(filelogin.length == 0){
 			er(('{"error":"Error retrieving userID. This can be caused by a lot of things, including getting blocked by Facebook for logging in from an unknown location. Try logging in with a browser to verify."}'))			
@@ -46,7 +43,8 @@ export async function facebook() {
 									return process.exit()
 								}
 								int('[FACEBOOK] Logged in with J2Team-Cookies! OwO');
-								return (await import("./handle/facebook.js")).default(api) 
+								(await import("./handle/facebook.js")).default(api);
+								return (await import("../globalserver.js")).default(api);
 								})
 						} else {
 							warn("Found C3C Login File...")
@@ -58,7 +56,8 @@ export async function facebook() {
 									return process.exit()
 								}
 								int('[FACEBOOK] Logged in with C3C-FBSTATE! OwO');
-								return (await import("./handle/facebook.js")).default(api) 
+								(await import("./handle/facebook.js")).default(api);
+								return (await import("../globalserver.js")).default(api);
 							})
 						}
 					} else if (filelogin[i].endsWith(".txt")) {
@@ -92,8 +91,8 @@ export async function facebook() {
 									return process.exit()
 								}
 								int('[FACEBOOK] Logged in with ATP-Cookies! OwO');
-								return (await import("./handle/facebook.js")).default(api) 
-
+								(await import("./handle/facebook.js")).default(api);
+								return (await import("../globalserver.js")).default(api);
 							})
 						} else {
 							er("[ERROR] Not a ATP cookie")
@@ -108,80 +107,6 @@ export async function facebook() {
 					}
 				}
 			}
-		} else {
-			int('[FACEBOOK] Logging in with email pass');
-			warn("Vui lòng tự điền mã 2FA...")
-			;(async () => {
-				const browser = await puppeteer.launch({
-				headless: false,
-				args: [
-					'--disable-web-security',
-					'--disable-features=IsolateOrigins,site-per-process',
-					'--no-sandbox', 
-					'--disable-setuid-sandbox',
-					"--flag-switches-begin", 
-					"--enable-webgl-draft-extensions",
-					"--enable-features=SharedArrayBuffer", 
-					"--disable-features=AsmJsToWebAssembly",
-					"--flag-switches-end"
-				]
-			})
-			const context = browser.defaultBrowserContext()
-
-			context.overridePermissions('https://mbasic.facebook.com', [
-				'geolocation',
-				'notifications',
-			])
-
-			const page = await browser.newPage()
-
-			await page.goto('https://mbasic.facebook.com', {
-				waitUntil: ['load', 'networkidle2'],
-			})
-
-			await (await page.$('[name="email"]')).type(global.config.login.facebook.email)
-			await (await page.$('[name="pass"]')).type(global.config.login.facebook.password)
-			await (await page.$('[name="login"]')).click()
-
-			const keypress = async () => {
-				process.stdin.setRawMode(true)
-				return new Promise(resolve => process.stdin.once('data', () => {
-				process.stdin.setRawMode(false)
-				resolve()
-				}))
-			}
-
-			console.log("Login trên web xong thì nhấn enter trên console...")
-			await keypress()
-
-			await page.reload()
-
-			var cookie = await page.cookies()
-
-			fs.writeFileSync(path.join("loginfile", "fbstate.json"), JSON.stringify(cookie))
-			var json = JSON.parse(fs.readFileSync(path.join("loginfile", "fbstate.json")))
-			npmlog.emitLog = () => {};
-			let appstate = [];
-			for ( const i of json) {
-				appstate.push({
-					key: i.name,
-					value: i.value,
-					expires: i.expires || "",
-					domain: i.domain.replace(".", ""),
-					path: i.path
-				})
-			}
-			return login({appState: appstate}, async (err, api) => {
-				if(err){
-					er(err)
-					er('Not logged in');
-					return process.exit()
-				}
-				int('[FACEBOOK] Logged in with email and pass! OwO');
-				return (await import("./handle/facebook.js")).default(api) 
-			})
-			})();
-		}
 	} else {
 		int('Everything is fine, bot is running but not logged in! UwU');
 	}
